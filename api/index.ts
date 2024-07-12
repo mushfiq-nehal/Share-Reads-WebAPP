75332a15b541d904560fa6c4ca570acb7a5b1280
DurjoyGH <200120.cse@student.just.edu.bd>
2024-07-12 11:51:56 +0600

    changes

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.render("login"));
 
 app.listen(3300, () => console.log("Server ready on port 3300."));
 
